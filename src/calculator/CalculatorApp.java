105aeb36d32195ed327ea6aab5e9be423a67f7f1
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2025-06-11 11:55:27 +0200

    changed the parameters for salaryCalc and added metod

--- src/calculator/CalculatorApp.java
+++ src/calculator/CalculatorApp.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 
 public class CalculatorApp {
     public static void main(String[] args) {
-        String newLine = System.lineSeparator();
+        SalaryCalculator massage = new SalaryCalculator();
+        massage.printText();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hej här kan du få hjälp att räkna ut din baklänges moms."+ newLine + " Ange faktura Belopp: ");
         int salary = scanner.nextInt();
 
+        SalaryCalculation result = SalaryCalculator.calculateSalary(salary);
 
-
-
-        SalaryCalculation result = SalaryCalculator.calculateSalary(salary, 1, 25.0);
-        System.out.println("Nettolön: " + result.netSalary + " kr" + newLine + "avgift: " + result.administrationFee + newLine + "moms: " +result.vat);
+        System.out.println("Netto lön" + result.netSalary);
 
 
     }
--- src/calculator/SalaryCalculator.java
+++ src/calculator/SalaryCalculator.java
@@ -2,16 +2,15 @@ package calculator;
 public class SalaryCalculator {
 
     public static SalaryCalculation calculateSalary(
-            int invoiceAmountExVAT,
-            double administrationFeePercentage,
-            double taxRatePercentage
+            int invoiceAmountExVAT
+
     ) {
         double taxPercentage = 0.3;
         double socialChargeCalculatedBackWord = 0.2391;
         int vat = Math.round((float) (invoiceAmountExVAT * 0.25));
         int invoiceAmount = invoiceAmountExVAT + vat;
 
-        int administrationFee = Math.round((float) (invoiceAmountExVAT * (administrationFeePercentage / 100)));
+        int administrationFee = Math.round((float) (invoiceAmountExVAT * (2.9 / 100)));
         int invoiceAfterFees = invoiceAmountExVAT - administrationFee;
 
         int employeeSocialCharges = (int) (invoiceAfterFees * socialChargeCalculatedBackWord);
@@ -31,5 +30,9 @@ public class SalaryCalculator {
                 salaryTax,
                 netSalary
         );
+    }
+    public void printText() {
+        System.out.println("Hej här kan du få hjälp att räkna ut din lön som egenanställd" +
+                '\n' + "Ange faktura Belopp: ");
     }
 }
