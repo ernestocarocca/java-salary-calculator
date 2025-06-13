b92c611d1417a9aa4e6b87718a32c513877d269c
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2025-06-13 21:24:48 +0200

    added a calculator

--- src/calculator/CalculatorApp.java
+++ src/calculator/CalculatorApp.java
@@ -1,14 +1,16 @@
 package calculator;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class CalculatorApp {
     public static void main(String[] args) {
-        SalaryCalculator massage = new SalaryCalculator();
+        /*
+    SalaryCalculator massage = new SalaryCalculator();
 
         Scanner scanner = new Scanner(System.in);
         int salary = 0;
-        while (true) {
+        do  {
             massage.printText();
             if (scanner.hasNextInt()) {
                 salary = scanner.nextInt();
@@ -19,6 +21,43 @@ public class CalculatorApp {
                 System.out.println("❌ Faktura Beloppet behöver vara en siffra.");
                 scanner.next();
             }
+        }while (true);
+        */
+
+        Calculator calc = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean continueRunning = true;
+        System.out.println("chose what you whana use");
+        System.out.println("1.Add");
+        System.out.println("2.Substract");
+        System.out.println("3.multiply");
+        System.out.println("4.Divide");
+        double result = 0;
+        int choice = scanner.nextInt();
+        double num1, num2;
+        System.out.println("enter you first number");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+        switch (choice) {
+            case 1:
+                result = calc.add(  num1, num2);
+            System.out.println("result" + result);
+            break;
+            case 2:
+                result = calc.subtract(  num1, num2);
+                System.out.println("result" + result);
+                break;
+            case 3:
+                result = calc.multiply(  num1, num2);
+                System.out.println("result" + result);
+            case 4:
+                result = calc.divide(  num1, num2);
+                System.out.println("result" + result);
         }
+
+
     }
-    }
+}
